7980ae451faeb9718aea39770a695dabef06b93a
DuyNguyen <duyyb257946@gmail.com>
2024-03-26 15:47:51 +0700

    generate role

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { routes } from "./src/routes/routes";
+import { generateRole } from "./src/tool/role";
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use("", routes);
+
 app.listen(process.env.PORT, () => {
   console.log(`Running http://localhost:${process.env.PORT}`);
 });
